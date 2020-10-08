5f09626779335f71dbfedd867ddc72bf7c531732
tzyLee <b06901145@ntu.edu.tw>
2020-10-09 01:06:53 +0800

    Run formatter on server.c

--- server.c
+++ server.c
@@ -1,35 +1,39 @@
-#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 #include <sys/socket.h>
-#include <fcntl.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include <unistd.h>
 
-#define ERR_EXIT(a) do { perror(a); exit(1); } while(0)
+#define ERR_EXIT(a) \
+    do {            \
+        perror(a);  \
+        exit(1);    \
+    } while (0)
 
 typedef struct {
-    char hostname[512];  // server's hostname
+    char hostname[512];   // server's hostname
     unsigned short port;  // port to listen
-    int listen_fd;  // fd to wait for a new connection
+    int listen_fd;        // fd to wait for a new connection
 } server;
 
 typedef struct {
     char host[512];  // client's host
-    int conn_fd;  // fd to talk with client
-    char buf[512];  // data sent by/to client
+    int conn_fd;     // fd to talk with client
+    char buf[512];   // data sent by/to client
     size_t buf_len;  // bytes used by buf
     // you don't need to change this.
     int id;
     int wait_for_write;  // used by handle_read to know if the header is read or not.
 } request;
 
-server svr;  // server
+server svr;                // server
 request* requestP = NULL;  // point to a list of requests
-int maxfd;  // size of open file descriptor table, size of request list
+int maxfd;                 // size of open file descriptor table, size of request list
 
 const char* accept_read_header = "ACCEPT_FROM_READ";
 const char* accept_write_header = "ACCEPT_FROM_WRITE";
@@ -44,7 +48,7 @@ static void free_request(request* reqP);
 // free resources used by a request instance
 
 typedef struct {
-    int id; //customer id
+    int id;  //customer id
     int adultMask;
     int childrenMask;
 } Order;
@@ -57,7 +61,6 @@ int handle_read(request* reqP) {
 }
 
 int main(int argc, char** argv) {
-
     // Parse args.
     if (argc != 2) {
         fprintf(stderr, "usage: %s [port]\n", argv[0]);
@@ -73,21 +76,22 @@ int main(int argc, char** argv) {
     int buf_len;
 
     // Initialize server
-    init_server((unsigned short) atoi(argv[1]));
+    init_server((unsigned short)atoi(argv[1]));
 
     // Loop for handling connections
     fprintf(stderr, "\nstarting on %.80s, port %d, fd %d, maxconn %d...\n", svr.hostname, svr.port, svr.listen_fd, maxfd);
 
     while (1) {
         // TODO: Add IO multiplexing
-        
+
         // Check new connection
         clilen = sizeof(cliaddr);
         conn_fd = accept(svr.listen_fd, (struct sockaddr*)&cliaddr, (socklen_t*)&clilen);
         if (conn_fd < 0) {
-            if (errno == EINTR || errno == EAGAIN) continue;  // try again
+            if (errno == EINTR || errno == EAGAIN)
+                continue;  // try again
             if (errno == ENFILE) {
-                (void) fprintf(stderr, "out of file descriptor table ... (maxconn %d)\n", maxfd);
+                (void)fprintf(stderr, "out of file descriptor table ... (maxconn %d)\n", maxfd);
                 continue;
             }
             ERR_EXIT("accept");
@@ -96,21 +100,21 @@ int main(int argc, char** argv) {
         strcpy(requestP[conn_fd].host, inet_ntoa(cliaddr.sin_addr));
         fprintf(stderr, "getting a new request... fd %d from %s\n", conn_fd, requestP[conn_fd].host);
 
-        int ret = handle_read(&requestP[conn_fd]); // parse data from client to requestP[conn_fd].buf
+        int ret = handle_read(&requestP[conn_fd]);  // parse data from client to requestP[conn_fd].buf
         if (ret < 0) {
             fprintf(stderr, "bad request from %s\n", requestP[conn_fd].host);
             continue;
         }
 
-    // TODO: handle requests from clients
-#ifdef READ_SERVER      
+        // TODO: handle requests from clients
+#ifdef READ_SERVER
         fprintf(stderr, "%s", requestP[conn_fd].buf);
-        sprintf(buf,"%s : %s",accept_read_header,requestP[conn_fd].buf);
-        write(requestP[conn_fd].conn_fd, buf, strlen(buf));    
-#else 
+        sprintf(buf, "%s : %s", accept_read_header, requestP[conn_fd].buf);
+        write(requestP[conn_fd].conn_fd, buf, strlen(buf));
+#else
         fprintf(stderr, "%s", requestP[conn_fd].buf);
-        sprintf(buf,"%s : %s",accept_write_header,requestP[conn_fd].buf);
-        write(requestP[conn_fd].conn_fd, buf, strlen(buf));    
+        sprintf(buf, "%s : %s", accept_write_header, requestP[conn_fd].buf);
+        write(requestP[conn_fd].conn_fd, buf, strlen(buf));
 #endif
 
         close(requestP[conn_fd].conn_fd);
@@ -146,7 +150,8 @@ static void init_server(unsigned short port) {
     svr.port = port;
 
     svr.listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (svr.listen_fd < 0) ERR_EXIT("socket");
+    if (svr.listen_fd < 0)
+        ERR_EXIT("socket");
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
@@ -165,7 +170,7 @@ static void init_server(unsigned short port) {
 
     // Get file descripter table size and initize request table
     maxfd = getdtablesize();
-    requestP = (request*) malloc(sizeof(request) * maxfd);
+    requestP = (request*)malloc(sizeof(request) * maxfd);
     if (requestP == NULL) {
         ERR_EXIT("out of memory allocating all requests");
     }
