e78f941bbd69b82f8da84c0d6a47c3bd002682c8
tzyLee <b06901145@ntu.edu.tw>
2020-10-13 00:35:10 +0800

    Add prompt

--- server.c
+++ server.c
@@ -29,17 +29,23 @@ typedef struct {
     size_t buf_len;  // bytes used by buf
     // you don't need to change this.
     int id;
-    int wait_for_write;  // used by handle_read to know if the header is read or not.
+    int wait_for_write;  // used by handle_read to know if the header is read or
+                         // not.
 } request;
 
-server svr;                 // server
-request* requestP = NULL;   // point to a list of requests
-int maxfd;                  // size of open file descriptor table, size of request list
+server svr;                // server
+request* requestP = NULL;  // point to a list of requests
+int maxfd;  // size of open file descriptor table, size of request list
 struct pollfd* fds = NULL;  // point to a list of pollfds
 int fds_len = 0;            // number of pollfds used in fds
 
 const char* accept_read_header = "ACCEPT_FROM_READ";
 const char* accept_write_header = "ACCEPT_FROM_WRITE";
+const char* id_prompt =
+    "Please enter the id (to check how many masks can be ordered):";
+const char* order_prompt =
+    "Please enter the mask type (adult or children) and number of mask you "
+    "would like to order:";
 
 static void init_server(unsigned short port);
 // initailize a server, exit for error
@@ -50,7 +56,11 @@ static void init_request(request* reqP);
 static void free_request(request* reqP);
 // free resources used by a request instance
 
-static void block_and_accept_connection(int sockfd, struct sockaddr_in* cliaddrp, int* clilen);
+static void block_and_accept_connection(int sockfd,
+                                        struct sockaddr_in* cliaddrp,
+                                        int* clilen);
+
+static int find_id_in_file(int id, const Order* order);
 
 static void append_fd(int fd);
 // append a fd to the end of fds
@@ -59,7 +69,7 @@ static void remove_fd(int idx);
 // remove fds[idx]
 
 typedef struct {
-    int id;  //customer id
+    int id;  // customer id
     int adultMask;
     int childrenMask;
 } Order;
@@ -82,6 +92,7 @@ int main(int argc, char** argv) {
     }
 
     struct sockaddr_in cliaddr;  // used by accept()
+    Order order;
     int clilen = sizeof(cliaddr);
 
     int conn_fd;  // fd for a new connection with client
@@ -97,10 +108,10 @@ int main(int argc, char** argv) {
     fds_len = 0;
 
     // Loop for handling connections
-    fprintf(stderr, "\nstarting on %.80s, port %d, fd %d, maxconn %d...\n", svr.hostname, svr.port, svr.listen_fd, maxfd);
+    fprintf(stderr, "\nstarting on %.80s, port %d, fd %d, maxconn %d...\n",
+            svr.hostname, svr.port, svr.listen_fd, maxfd);
 
     append_fd(svr.listen_fd);
-    fds[0].events = POLLIN;  // check for incoming connections
 
     while (1) {
         // TODO: Add IO multiplexing
@@ -109,7 +120,7 @@ int main(int argc, char** argv) {
             block_and_accept_connection(svr.listen_fd, &cliaddr, &clilen);
         } else {
             // Has Pending requests
-            if ((total_fd = poll(fds, fds_len, 1000)) < 0) {
+            if ((total_fd = poll(fds, fds_len, -1)) < 0) {
                 ERR_EXIT("poll");
             }
             if (total_fd == 0)
@@ -129,21 +140,48 @@ int main(int argc, char** argv) {
                     continue;
                 }
                 int fd = fds[i].fd;
-                int ret = handle_read(&requestP[fd]);  // parse data from client to requestP[conn_fd].buf
+                int ret =
+                    handle_read(&requestP[fd]);  // parse data from client to
+                                                 // requestP[conn_fd].buf
                 if (ret < 0) {
                     fprintf(stderr, "bad request from %s\n", requestP[fd].host);
                     continue;
                 }
-                fprintf(stderr, "handling request %d, %d can be read\n", i, total_fd);
+                fprintf(stderr, "handling request %d, %d can be read\n", i,
+                        total_fd);
+                if (ret != 0) {
+                    int id = atoi(requestP[fd].buf);
+                    if (find_id_in_file(id, &order) == -1) {
+                        write(requestP[fd].conn_fd, "Operation failed.",
+                              strlen("Operation failed."));
+                        close(requestP[fd].conn_fd);
+                        free_request(&requestP[fd]);
+                        remove_fd(i);
+                        if (--total_fd == 0)
+                            break;
+                        continue;
+                    } else {
+                        sprintf(buf,
+                                "You can order %d adult mask(s) and %d "
+                                "children mask(s).",
+                                order.adultMask, order.childrenMask);
+                        write(requestP[fd].conn_fd, buf, strlen(buf));
 #ifdef READ_SERVER
-                fprintf(stderr, "%s", requestP[fd].buf);
-                sprintf(buf, "%s : %s", accept_read_header, requestP[fd].buf);
-                write(requestP[fd].conn_fd, buf, strlen(buf));
 #else
-                fprintf(stderr, "%s", requestP[fd].buf);
-                sprintf(buf, "%s : %s", accept_write_header, requestP[fd].buf);
-                write(requestP[fd].conn_fd, buf, strlen(buf));
 #endif
+                    }
+#ifdef READ_SERVER
+                    fprintf(stderr, "%s", requestP[fd].buf);
+                    sprintf(buf, "%s : %s", accept_read_header,
+                            requestP[fd].buf);
+                    write(requestP[fd].conn_fd, buf, strlen(buf));
+#else
+                    fprintf(stderr, "%s", requestP[fd].buf);
+                    sprintf(buf, "%s : %s", accept_write_header,
+                            requestP[fd].buf);
+                    write(requestP[fd].conn_fd, buf, strlen(buf));
+#endif
+                }
                 close(requestP[fd].conn_fd);
                 free_request(&requestP[fd]);
                 remove_fd(i);
@@ -159,10 +197,13 @@ int main(int argc, char** argv) {
 }
 
 // ======================================================================================================
-static void block_and_accept_connection(int sockfd, struct sockaddr_in* cliaddrp, int* clilen) {
+static void block_and_accept_connection(int sockfd,
+                                        struct sockaddr_in* cliaddrp,
+                                        int* clilen) {
     int conn_fd = 0;
     do {
-        conn_fd = accept(sockfd, (struct sockaddr*)cliaddrp, (socklen_t*)clilen);
+        conn_fd =
+            accept(sockfd, (struct sockaddr*)cliaddrp, (socklen_t*)clilen);
         // success
         if (conn_fd >= 0)
             break;
@@ -172,8 +213,11 @@ static void block_and_accept_connection(int sockfd, struct sockaddr_in* cliaddrp
         case EAGAIN:
             continue;  // try again
         case ENFILE:
-            (void)fprintf(stderr, "out of file descriptor table ... (maxconn %d)\n", maxfd);
-            return;  // Stop accept new connection to allow the requests to be done
+            (void)fprintf(stderr,
+                          "out of file descriptor table ... (maxconn %d)\n",
+                          maxfd);
+            return;  // Stop accept new connection to allow the requests to be
+                     // done
         default:
             ERR_EXIT("accept");
         }
@@ -181,13 +225,33 @@ static void block_and_accept_connection(int sockfd, struct sockaddr_in* cliaddrp
     requestP[conn_fd].conn_fd = conn_fd;
     strcpy(requestP[conn_fd].host, inet_ntoa(cliaddrp->sin_addr));
     append_fd(conn_fd);
-    fprintf(stderr, "getting a new request... fd %d from %s\n", conn_fd, requestP[conn_fd].host);
+    fprintf(stderr, "getting a new request... fd %d from %s\n", conn_fd,
+            requestP[conn_fd].host);
+    // prompt for id
+    write(conn_fd, id_prompt, strlen(id_prompt));
+}
+
+static int find_id_in_file(int id, const Order* order) {
+    static struct flock lock = {
+        .l_type = F_RDLCK, .l_start = 0, .l_whence = SEEK_SET, .l_len = 0};
+    int fd = open("./preorderRecord", O_RDONLY);
+    while (fcntl(fd, F_SETLKW, &lock) == -1 && errno == EACCES) {
+        // acquire read lock
+    }
+    int res = 0;
+    while ((res = read(fd, order, sizeof(Order))) > 0) {
+        if (order->id == id) {
+            break;
+        }
+    }
+    close(fd);
+    return -1;  // id not found in file
 }
 
 static void append_fd(int fd) {
     // assume 0 <= fds_len < maxfds;
     fds[fds_len].fd = fd;
-    fds[fds_len].events = POLLIN | POLLHUP;  // can be read or hung up
+    fds[fds_len].events = POLLIN;  // can be read or hung up
     fds[fds_len].revents = 0;
     ++fds_len;
 }
@@ -234,10 +298,12 @@ static void init_server(unsigned short port) {
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(port);
     tmp = 1;
-    if (setsockopt(svr.listen_fd, SOL_SOCKET, SO_REUSEADDR, (void*)&tmp, sizeof(tmp)) < 0) {
+    if (setsockopt(svr.listen_fd, SOL_SOCKET, SO_REUSEADDR, (void*)&tmp,
+                   sizeof(tmp)) < 0) {
         ERR_EXIT("setsockopt");
     }
-    if (bind(svr.listen_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+    if (bind(svr.listen_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) <
+        0) {
         ERR_EXIT("bind");
     }
     if (listen(svr.listen_fd, 1024) < 0) {
